da220693e50658b6ef3b9f5783b064035246fc15
fadeawayQAQ <2678636941@qq.com>
2023-09-27 20:09:33 +0800

    0.4

--- magicClock.js
+++ magicClock.js
@@ -21,9 +21,7 @@ let hourHandRotate = {
 };
 const hourHand = new Image();
 hourHand.src = "./src/HourHand.png";
-hourHand.onload = () => {
-    hourHandDraw();
-}
+hourHand.onload = () => {}
 
 const minuteHandPos = {left: 97, top: 470, width: 7, height: 79};
 const minuteHandCenter = {left: 4, top:65};
@@ -33,9 +31,7 @@ let minuteHandRotate = {
 };
 const minuteHand = new Image();
 minuteHand.src = "./src/MinuteHand.png";
-minuteHand.onload = () => {
-    minuteHandDraw();
-}
+minuteHand.onload = () => {}
 
 
 let isDown = false;
@@ -43,13 +39,15 @@ let now = {x: -1, y: -1};
 let boltPos = {left: 75, top: 100, width: 50, height: 120};
 const Bolt = new Image();
 Bolt.src = "./src/Bolt.png";
-Bolt.onload = () => {
-    _mgCtx_.drawImage(Bolt, boltPos.left, boltPos.top, boltPos.width, boltPos.height);
-}
+Bolt.onload = () => {}
 Bolt.style.position = "absolute";
 
-
 updatePos(boltPos.left, boltPos.top);
+
+
+updaterad();
+drawAll();
+
 _magicCvs_.onmousedown = e => {
     isDown = true;     
     // 鼠标按下时，设置isDown为true，此时移动鼠标才认为是有效。
@@ -123,12 +121,20 @@ function minuteHandDraw() {
         left: minuteHandCenter.left * Math.cos(rad) - minuteHandCenter.top * Math.sin(rad),
         top: minuteHandCenter.top * Math.cos(rad) + minuteHandCenter.left * Math.sin(rad)
     };
-    console.log("坐标:", nwc.left, nwc.top);
+    //console.log("坐标:", nwc.left, nwc.top);
     _mgCtx_.translate(center.left - nwc.left, center.top - nwc.top);
     _mgCtx_.rotate(rad);
     _mgCtx_.drawImage(minuteHand, 0, 0, minuteHandPos.width, minuteHandPos.height);
     _mgCtx_.rotate(- rad);
     _mgCtx_.translate(- center.left + nwc.left, - center.top + nwc.top);
+}
+function updaterad() {
+    let nwTime = getTime();
+    let nwH = parseInt(nwTime.slice(0, 2));
+    let nwM = parseInt(nwTime.slice(3, 5));
+    let nwMinute = nwH * 60 + nwM;
+    hourHandRotate.rad = Math.PI * 2 * nwMinute / 1440.0;
+    minuteHandRotate.rad = Math.PI * 2 * nwM / 60.0;
 }
 function drawAll() {
     //draw所有元素
@@ -136,6 +142,7 @@ function drawAll() {
     _mgCtx_.drawImage(Bolt, boltPos.left, boltPos.top, boltPos.width, boltPos.height);
     _mgCtx_.drawImage(clockBoard, clockBoardPos.left, clockBoardPos.top, clockBoardPos.width, clockBoardPos.height);
 
+    updaterad();
     hourHandDraw();
     minuteHandDraw();
 
